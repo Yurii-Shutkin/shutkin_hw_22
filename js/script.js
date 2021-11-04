f7828cfcc1b0ec4914610d710a938519f2b37ac1
Yurii-Shutkin <itorik@ukr.net>
2021-11-04 20:46:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const getPalindrom = function(num, stepsArr = []) {
+const getPalindrom = function(num, stepsCounter = 0) {
     const obj = {};
     let arr = [num];
     arr = arr.join().split('');
     
     if(arr.join() === arr.reverse().join()) {
-        obj.steps = stepsArr;
         obj.result = num;
+        obj.steps = stepsCounter;
         return obj;
     }
     arr = +arr.join('');
-    stepsArr.push(num);
-    return getPalindrom(num + arr, stepsArr);
+    stepsCounter += 1;
+    return getPalindrom(num + arr, stepsCounter);
 }
 
 console.log(getPalindrom(96));
